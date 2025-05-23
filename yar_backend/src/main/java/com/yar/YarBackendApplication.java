201fca19e88596c29cffa236516b8a237eda8e7b
Janghawnhui <68009882+Janghawnhui@users.noreply.github.com>
2025-05-24 01:54:18 +0900

    백엔드파일구조변경

---
+++ yar_backend/src/main/java/com/yar/YarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YarBackendApplication.class, args);
+    }
+
+}
---
+++ yar_backend/src/test/java/com/yar/YarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
