527abb40ba44b722bc3b7f6343541331f6c69f2c
unknown <ghksgml0807@naver.com>
2025-06-04 12:16:02 +0900

    병합 진짜완료

--- back/src/main/java/com/yar/back/entity/Stock.java
+++ back/src/main/java/com/yar/back/entity/Stock.java
@@ -1,5 +1,6 @@
 package com.yar.back.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -8,12 +9,27 @@ import jakarta.persistence.Table;
 @Table(name = "company")
 public class Stock {
 
+
     @Id
+    @Column(name = "stock_code")
+    private String stockCode; // 종목코드는 고유하므로 ID로 지정
+
+    @Column(name = "company_name")
     private String companyName;
 
-    private String stockCode;
+    private Long marketCap;
+
+    private String marketType;
 
     // Getter/Setter
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
     public String getCompanyName() {
         return companyName;
     }
@@ -22,11 +38,16 @@ public class Stock {
         this.companyName = companyName;
     }
 
-    public String getStockCode() {
-        return stockCode;
+    public Long getMarketCap() {
+        return marketCap;
     }
-
-    public void setStockCode(String stockCode) {
-        this.stockCode = stockCode;
+    public void setMarketCap(Long marketCap) {
+        this.marketCap = marketCap;
+    }
+    public String getMarketType() {
+        return marketType;
+    }
+    public void setMarketType(String marketType) {
+        this.marketType = marketType;
     }
 }
--- back/src/main/java/com/yar/back/service/StockService.java
+++ back/src/main/java/com/yar/back/service/StockService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Service
 public class StockService {
@@ -50,4 +51,12 @@ public class StockService {
             e.printStackTrace();
         }
     }
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    public List<Stock> searchStockByName(String name) {
+        return stockRepository.findByCompanyName(name);
+    }
+
 }
