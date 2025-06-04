eae4722f3c6aef508e32d518284c5fa354bab025
문준영 <answnsdud1004@naver.com>
2025-06-04 13:16:29 +0900

    Search DESC

--- back/src/main/java/com/yar/back/repository/StockRepository.java
+++ back/src/main/java/com/yar/back/repository/StockRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface StockRepository extends JpaRepository<Stock, String> {
-    @Query("SELECT s FROM Stock s WHERE s.companyName LIKE %:name%")
+
+    @Query("SELECT s FROM Stock s WHERE s.companyName LIKE %:name% ORDER BY " +
+            "CASE WHEN s.companyName = :name THEN 0 ELSE 1 END, s.marketCap DESC")
     List<Stock> findByCompanyName(@Param("name") String name);
+
 }
